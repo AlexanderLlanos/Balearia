f74f6b1f09eb3ef52dfb78971576a2d902204f00
AlexanderLlanos <alexander.asdfg@gmail.com>
2025-07-16 14:18:07 -0500

    story

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: [
+    '../src/components/**/*.stories.js',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
+  framework: '@storybook/html',
+}; 
